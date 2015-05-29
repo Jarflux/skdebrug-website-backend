ff9e3e153474a2c7414317ca9f9284bf72ed3097
Ben Oeyen <ben.oeyen@xplore.be>
2015-05-29 14:58:24 +0200

    Added Model News Added NewsRepository implementation

--- src/main/java/be/skdebrug/website/core/News.java
+++ src/main/java/be/skdebrug/website/core/News.java
@@ -4,4 +4,22 @@ package be.skdebrug.website.core;
  * Created by Ben on 24/05/15.
  */
 public class News {
+    int id;
+    String content;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
--- src/main/java/be/skdebrug/website/repository/NewsRepository.java
+++ src/main/java/be/skdebrug/website/repository/NewsRepository.java
@@ -1,7 +1,124 @@
 package be.skdebrug.website.repository;
 
+import be.skdebrug.website.core.News;
+import be.skdebrug.website.endpoint.SQLiteConnection;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Ben on 24/05/15.
  */
 public class NewsRepository {
+    private static final String TBL_NEWS = "tbl_news";
+    private static final String COL_NEWS_ID = "newsID";
+    private static final String COL_NEWS_CONTENT = "content";
+
+    public static boolean dropDatabaseOnInjection = false;
+
+    public static void setDatabaseLocation(String path) {
+        SQLiteConnection.databaseLocation = path;
+    }
+
+    private News parseNewsFromResult(ResultSet queryResult) throws SQLException{
+        News news = new News();
+        news.setId(queryResult.getInt(COL_NEWS_ID));
+        news.setContent(queryResult.getString(COL_NEWS_CONTENT));
+        return news;
+    }
+
+    public NewsRepository() {
+        if (dropDatabaseOnInjection) {
+            (new SQLiteConnection<Boolean>() {
+                @Override
+                public Boolean defineOperation(Statement statement) throws SQLException {
+                    statement.execute(log("DROP TABLE IF EXISTS " + TBL_NEWS));
+                    return true;
+                }
+            }).runOperation();
+        }
+        (new SQLiteConnection<Boolean>() {
+            @Override
+            public Boolean defineOperation(Statement statement) throws SQLException {
+                statement.execute(log("CREATE TABLE IF NOT EXISTS " + TBL_NEWS + " (" + COL_NEWS_ID + " INTEGER PRIMARY KEY ASC," + COL_NEWS_CONTENT + "," +
+                        "CONSTRAINT unique_" + TBL_NEWS + " UNIQUE(" + COL_NEWS_ID + ") ON CONFLICT IGNORE );"));
+                return true;
+            }
+        }).runOperation();
+    }
+
+    public boolean create(final News news) {
+        return (new SQLiteConnection<Boolean>() {
+            @Override
+            public Boolean defineOperation(Statement statement) throws SQLException {
+                statement.executeUpdate(log("INSERT INTO " + TBL_NEWS + " (" + COL_NEWS_CONTENT + ") "
+                        + "VALUES ('" + news.getContent() + "');"));
+                return true;
+            }
+        }).runOperation();
+    }
+
+    public News read(final String newsId) {
+        return (new SQLiteConnection<News>() {
+            @Override
+            public News defineOperation(Statement statement) throws SQLException {
+                ResultSet queryResult = statement.executeQuery(log("SELECT * FROM " + TBL_NEWS + " WHERE " + COL_NEWS_ID + " = '" + newsId + "'"));
+                if (queryResult.next()) {
+                    News news = new News();
+                    news.setId(queryResult.getInt(COL_NEWS_ID));
+                    news.setContent(queryResult.getString(COL_NEWS_CONTENT));
+                    return news;
+                }
+                return null;
+            }
+        }).runOperation();
+    }
+
+    public List<News> readAll() {
+        return (new SQLiteConnection<List<News>>() {
+            @Override
+            public List<News> defineOperation(Statement statement) throws SQLException {
+                List<News> newsList = new ArrayList<>();
+                ResultSet queryResult = statement.executeQuery(log("SELECT * FROM " + TBL_NEWS));
+                while (queryResult.next()) {
+                    newsList.add(parseNewsFromResult(queryResult));
+                }
+                return newsList;
+            }
+        }).runOperation();
+    }
+
+    public boolean update(final News news) {
+        return (new SQLiteConnection<Boolean>() {
+            @Override
+            public Boolean defineOperation(Statement statement) throws SQLException {
+                statement.executeUpdate(log("INSERT INTO " + TBL_NEWS + " (" + COL_NEWS_ID + "," + COL_NEWS_CONTENT + ") "
+                        + "VALUES ('" + news.getId() + "','" + news.getContent() + "');"));
+                return true;
+            }
+        }).runOperation();
+    }
+
+    public boolean delete(final String newsId) {
+        return (new SQLiteConnection<Boolean>() {
+            @Override
+            public Boolean defineOperation(Statement statement) throws SQLException {
+                statement.execute(log("DELETE FROM " + TBL_NEWS + " WHERE " + COL_NEWS_ID + " = '" + newsId + "'"));
+                return true;
+            }
+        }).runOperation();
+    }
+
+    public boolean deleteAll() {
+        return (new SQLiteConnection<Boolean>() {
+            @Override
+            public Boolean defineOperation(Statement statement) throws SQLException {
+                statement.execute(log("DELETE FROM " + TBL_NEWS ));
+                return true;
+            }
+        }).runOperation();
+    }
 }
